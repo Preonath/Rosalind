129472817858d70753380272186f04e8603abbda
Preonath Shuvo <preonath2838@gmail.com>
2023-10-08 16:51:14 +0600

    Remove Bioinformatics Textbook Track and Bioinformatics Stronghold directories

--- Bioinformatics Stronghold/R27 Partial Permutations.py
+++
@@ -1,6 +0,0 @@
-n,k=map(int,input().split())
-fact=1
-for i in range(k):
-    fact*=(n-i)
-par_modulo=fact%1000000
-print(par_modulo)
--- Bioinformatics Stronghold/R35 Counting Phylogenetic Ancestors.py
+++
@@ -1,3 +0,0 @@
-f=open('R35.txt')
-n = int(f.read().strip())
-print(n-2)
